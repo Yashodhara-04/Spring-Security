18f7949b5b1a259f9aea5b2f3e6f5a877afafc2b
Yashodhara C H <yashodhara.chowkar@gmail.com>
2024-09-02 09:18:59 +0530

    Change default csrf setting and create a customized security

---
+++ src/main/java/com/SpringSecure/SpringSecurity/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.SpringSecure.SpringSecurity.config;
+
+import jakarta.servlet.annotation.ServletSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain setSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .build();
+
+
+    }
+}
