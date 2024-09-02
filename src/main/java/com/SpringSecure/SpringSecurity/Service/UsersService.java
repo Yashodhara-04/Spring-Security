ad68b21e522906f9f5ae11699fd1b4a18a1678d0
Yashodhara C H <yashodhara.chowkar@gmail.com>
2024-09-02 16:45:01 +0530

    Encrypt the pasword using BCryptPasswordEncoder while saving and retriving password

--- src/main/java/com/SpringSecure/SpringSecurity/Service/UsersService.java
+++ src/main/java/com/SpringSecure/SpringSecurity/Service/UsersService.java
@@ -1,4 +1,22 @@
 package com.SpringSecure.SpringSecurity.Service;
 
+import com.SpringSecure.SpringSecurity.DataModel.Users;
+import com.SpringSecure.SpringSecurity.Repo.UsersRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsersService {
+
+    @Autowired
+    private UsersRepo usersRepo;
+    BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+    public Users saveUsers(Users users)
+    {
+        users.setPassword(bCryptPasswordEncoder.encode(users.getPassword()));
+        usersRepo.save(users);
+        return users;
+    }
 }
--- src/main/java/com/SpringSecure/SpringSecurity/controller/UsersController.java
+++ src/main/java/com/SpringSecure/SpringSecurity/controller/UsersController.java
@@ -1,4 +1,21 @@
 package com.SpringSecure.SpringSecurity.controller;
 
+import com.SpringSecure.SpringSecurity.DataModel.Users;
+import com.SpringSecure.SpringSecurity.Service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UsersController {
+
+    @Autowired
+    private UsersService usersService;
+
+    @PostMapping("/Users")
+    public Users addUserData(@RequestBody Users users)
+    {
+        return usersService.saveUsers(users);
+    }
 }
